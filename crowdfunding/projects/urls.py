248832625700657fa4981c820c39d34f2793061b
Monelle Trachez <monelle.trachez@gmail.com>
2022-04-28 16:35:33 +0800

    Change path for Category

--- crowdfunding/projects/urls.py
+++ crowdfunding/projects/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('projects/', views.ProjectList.as_view()),
     path('projects/<int:pk>', views.ProjectDetail.as_view()),
     path('pledges/', views.PledgeList.as_view()),
-    path('categories/', views.CategoryList.as_view())
+    path('categories/', views.CategoryList.as_view(), name='category-list'),
 
 ]
 
